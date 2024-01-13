90e334ace9fc3bf2a854d38514ea32ed22e8372b
Nicolai Klokmose <angryniczorn@gmail.com>
2024-01-13 15:56:00 +0100

    Keywords Passed

--- Keywords.java
+++ Keywords.java
@@ -3,14 +3,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Keywords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> lst = new ArrayList<String>();
         int n = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < n; i++) {
-            lst.add(sc.nextLine().toLowerCase());
+            lst.add(sc.nextLine().toLowerCase().replace("-", " "));
         }
 
         System.out.println(new HashSet<String>(lst).size());
